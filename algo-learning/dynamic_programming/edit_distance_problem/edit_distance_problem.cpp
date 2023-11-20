d4e2e06efb96148500a3ff180cadb103e40abfa7
PYQ <eilo.pengyq@gmail.com>
2023-11-20 10:42:48 +0800

    docs: edit_distance_problem

---
+++ algo-learning/dynamic_programming/edit_distance_problem/edit_distance_problem.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int editDistanceDP(string s, string t) {
+    int n = s.size(), m = t.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    for (int j = 1; j < m+1; j++)
+        dp[0][j] = j;
+    for (int i = 1; i < n+1; i++)
+        dp[i][0] = i;
+    for (int i = 1; i < n+1; i++) {
+        for (int j = 1; j < m+1; j++) {
+            if (s[i-1] == t[j-1])
+                dp[i][j] = dp[i-1][j-1];
+            else
+                dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1;
+        }
+    }
+    return dp[n][m];
+}
