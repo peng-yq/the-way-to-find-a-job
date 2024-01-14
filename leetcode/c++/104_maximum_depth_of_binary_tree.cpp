b5aa8958ce8817f0560449767302fe12733c2f7f
PYQ <eilo.pengyq@gmail.com>
2024-01-14 20:04:45 +0800

    docs: 104_maximum_depth_of_binary_tree

---
+++ leetcode/c++/104_maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.cn/problems/maximum-depth-of-binary-tree/description
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
+// 时间复杂度为O(n)，空间复杂度为O(n) （最差退化为链表
---
+++ leetcode/golang/104_maximum_depth_of_binary_tree.go
@@ -0,0 +1,12 @@
+type TreeNode struct {
+        Val int
+    Left *TreeNode
+        Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
