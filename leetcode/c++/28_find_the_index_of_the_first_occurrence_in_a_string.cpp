32f38185cf8161f2ef8dd24b174303cfecf8a05b
pyq <eilo.pengyq@gmail.com>
2024-06-12 17:38:52 +0800

    docs: 28_find_the_index_of_the_first_occurrence_in_a_string

---
+++ leetcode/c++/28_find_the_index_of_the_first_occurrence_in_a_string.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        for (int i = 0; i <= m - n; ++i) {
+            int j = 0;
+            while (j < n && haystack[i + j] == needle[j])
+                ++j;
+            if (j == n)
+                return i;
+        }
+        return -1;
+    }
+};
